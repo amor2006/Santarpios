e698d00209b158b73a601059f29a24c40934b112
amor2006 <36248164+amor2006@users.noreply.github.com>
2023-02-27 02:26:33 -0500

    f

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -11,42 +11,49 @@ import org.openqa.selenium.safari.SafariDriver;
 import utils.CommonUtils;
 
 public class DriverFactory {
-	
-	static WebDriver driver = null;
-	
-	public static WebDriver initializeBrowser(String browserName) {
-		
-		if(browserName.equals("chrome")) {
-			
-			driver = new ChromeDriver();
-			
-		}else if(browserName.equals("firefox")) {
-			
-			driver = new FirefoxDriver();
-			
-		}else if(browserName.equals("edge")) {
-			
-			driver = new EdgeDriver();
-			
-		}else if(browserName.equals("safari")) {
-			
-			driver = new SafariDriver();
-			
-		}
-		
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-		
-		return driver;
-		
-	}
-	
-	public static WebDriver getDriver() {
-		
-		return driver;
-		
-	}
+
+    static WebDriver driver = null;
+
+    public static WebDriver initializeBrowser(String browserName) {
+
+
+        if (browserName.equals("chrome")) {
+
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless", "--window-size=1920,1200", "--ignore-certificate-errors");
+
+            driver = new ChromeDriver(options);
+//			driver = new ChromeDriver();
+
+        } else if (browserName.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        } else if (browserName.equals("safari")) {
+            WebDriverManager.safaridriver().setup();
+            driver = new SafariDriver();
+
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(CommonUtils.PAGE_LOAD_TIME, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(CommonUtils.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+
+        return driver;
+
+    }
+
+    public static WebDriver getDriver() {
+
+        return driver;
+
+    }
 
 }
