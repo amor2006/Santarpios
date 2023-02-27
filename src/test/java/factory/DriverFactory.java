ba9ec77c61eb003856fd8f908dcb6b82b2821be8
amor2006 <36248164+amor2006@users.noreply.github.com>
2023-02-27 02:29:03 -0500

    Update DriverFactory.java

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -1,14 +1,15 @@
 package factory;
 
-import java.time.Duration;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
 import utils.CommonUtils;
+
+import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
 
